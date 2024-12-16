d4cc84dbd64b9b6f4fde8a73aac13589b759246f
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-16 17:46:20 +0200

    Add loop for sum calculation

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
         int sum = 0;
         int count = 0;
+
+        for (int i = 1; i <= 6; i++) {
+            count++;
+            sum += i;
+            System.out.println(count + ") Num is " + i + ", sum is " + sum);
+        }
+        System.out.println("----------------------" + "\nSum of numbers is " + sum);
     }
 }
